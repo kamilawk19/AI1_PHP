af0848590ced42241486ece7133de76949be7d1b
Yuliia Tarasenko <ty46715@zut.edu.pl>
2022-01-17 13:56:49 +0100

    Dodanie designu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,84 +1,51 @@
 <!doctype html>
-<html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="/css/main.css">
     <title><?php echo $this->title ?></title>
 </head>
 <body>
-<nav>
-    <a href="#">Navbar</a>
-    <div>
-        <ul>
-            <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
-            </li>
+    <header>
+        <a class="logo" href="/"><img src="/css/logo.svg" alt="logo"></a>
+    
+    <nav>
+        <ul class="nav__links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/about">About</a></li>
         </ul>
+    </nav>
 
-        <?php use app\core\Application;
+    <?php use app\core\Application;
 
-        if (Application::isGuest()): ?>
-            <ul>
-                <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                </li>
-            </ul>
-        <?php else: ?>
-            <ul">
-                <li class="nav-item">
-                    <a class="nav-link" href="/timer">
-                        Timer
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/projects">
-                        Projects
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/clients">
-                        Clients
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/team">
-                        Team
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile">
-                        Profile
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/logout">
-                        Welcome <?php echo Application::$app->user->getDisplayName() ?> (Logout)
-                    </a>
-                </li>
-            </ul>
-        <?php endif; ?>
+    if (Application::isGuest()): ?>
+    <div>
+        <a class="cta" href="/login">Login</a>
+        <a class="cta" href="/register">Register</a>
     </div>
-</nav>
+    
+    <?php else: ?>
+        <ul">
+            <li><a href="/timer">Timer</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/clients">Clients</a></li>
+            <li><a href="/team">Team</a></li>
+            <li><a href="/profile">Profile</a></li>
+            <li><a href="/logout">Welcome <?php echo Application::$app->user->getDisplayName() ?> (Logout)</a></li>
+        </ul>
+    <?php endif; ?>
+    </header>
 
-<div class="container">
     <?php if (Application::$app->session->getFlash('success')): ?>
         <div>
             <p><?php echo Application::$app->session->getFlash('success') ?></p>
         </div>
     <?php endif; ?>
-    {{content}}
-</div>
+    <div>{{content}}</div>
+
 
 </body>
-</html>
--- views/login.php
+++ views/login.php
@@ -1,16 +1,22 @@
-<?php
+<body class="box">
+    <?php
 
-/** @var $model \app\models\LoginForm */
-/** @var $this \app\core\View */
-use app\core\form\Form;
-$this->title = 'Login';
-?>
+    /** @var $model \app\models\LoginForm */
+    /** @var $this \app\core\View */
+    use app\core\form\Form;
+    $this->title = 'Login';
+    ?>
 
-
-<h1>Login</h1>
-
-<?php $form = Form::begin('', 'post') ?>
-    <?php echo $form->field($model, 'email') ?>
-    <?php echo $form->field($model, 'password')->passwordField() ?>
-    <button>Submit</button>
-<?php Form::end() ?>
+    <div class="container">
+        <h1 class="form__title">Login</h1>
+        <?php $form = Form::begin('', 'post') ?>
+            <div class="form__input-group">
+                <?php echo $form->field($model, 'email') ?>
+            </div>
+            <div class="form__input-group">
+                <?php echo $form->field($model, 'password')->passwordField() ?>
+            </div>
+            <button class="form__button" type="submit">Submit</button>
+        <?php Form::end() ?>
+    </div>
+</body>
